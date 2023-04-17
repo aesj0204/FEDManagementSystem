5f351ade1e549495a9487c767d76932717c2fa34
aesj0204 <127114276+aesj0204@users.noreply.github.com>
2023-04-17 20:16:09 +0900

    Update Food.java

--- FEDManagementSystem/src/xms/Food.java
+++ FEDManagementSystem/src/xms/Food.java
@@ -8,8 +8,18 @@ public class Food {
 	String expirydate;
 	String foodimage;
 	String note;
+	int dDay;
 	
 	public Food() {
+	}
+	
+	public Food(String foodname, String foodcategory, String location, String expirydate, String foodimage) {
+		this.foodname = foodname;
+		this.foodcategory = foodcategory;
+		this.location = location;
+		this.expirydate = expirydate;
+		this.foodimage = foodimage;
+		
 	}
 	
 	public Food(String foodname, String foodcategory, String location, String expirydate, String foodimage, String note) {
@@ -21,8 +31,23 @@ public class Food {
 		this.note = note;
 	}
 	
+	public String DDAY() {
+		String[] expirydate_1 = expirydate.split("/");
+		DdayConverter dday = new DdayConverter(expirydate_1[0], expirydate_1[1], expirydate_1[2]);
+		dDay = dday.getDDay();
+		if(dDay == 0) {
+			return "D-Day";
+		}
+		else if(dDay > 0) {
+			return "D-" + dDay;
+		}
+		else {
+			return "D+" +Math.abs(dDay);
+		}
+	}
+	
 	public void printInfo() {
-		System.out.println("foodname: " + this.foodname + " foodcategory: " + this.foodcategory + " location: " + this.location + " expirydate: " + this.expirydate + " foodimage: " + this.foodimage + " note: " + this.note);
+		System.out.println("foodname: " + this.foodname + " foodcategory: " + this.foodcategory + " location: " + this.location + " expirydate: " + this.expirydate + " foodimage: " + this.foodimage + " note: " + this.note + " D-Day: " + DDAY());
 	}
 
 }
