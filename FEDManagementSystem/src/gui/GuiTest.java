d149d270d50ed34db30a89a6f4e1f7fbe9658a0d
aesj0204 <leehj7978@gnu.ac.kr>
2023-06-05 18:36:04 +0900

    add

--- FEDManagementSystem/src/gui/GuiTest.java
+++ FEDManagementSystem/src/gui/GuiTest.java
@@ -3,8 +3,8 @@ package gui;
 public class GuiTest {
 
 	public static void main(String[] args) {
-		//MenuSelection menuselection = new MenuSelection();
-		//FoodAdder foodadder = new FoodAdder();
+		MenuSelection menuselection = new MenuSelection();
+		FoodAdder foodadder = new FoodAdder();
 		FoodViewer foodviewer = new FoodViewer();
 
 	}
